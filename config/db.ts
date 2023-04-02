f8ebd8d8ffe887b15972953f0a42f9572df14303
Okwoli-Godwin <okwolig60@gmail.com>
2023-04-02 14:52:26 +0100

    updated

--- config/db.ts
+++ config/db.ts
@@ -2,3 +2,12 @@ import mongoose from "mongoose";
 import { enviromentVariable } from "../enviromentvariable/enviromentvariable";
 
 const URL = enviromentVariable.MONGODB_STRING
+
+export const dbConfig = async() => {
+    try {
+        const conn = await mongoose.connect(URL)
+        console.log(`Database connected to ${conn.connection.host}`)
+    } catch (error) {
+        console.log("Failed to connect to the database")
+    }
+}
--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import express, { Application } from "express"
 import { AppConfig } from "./App"
+import { dbConfig } from "./config/db"
 import { enviromentVariable } from "./enviromentvariable/enviromentvariable"
 
 
 const app: Application = express()
 
 AppConfig(app)
+dbConfig()
 
 app.listen(enviromentVariable.port, () => {
     console.log("App is running")
