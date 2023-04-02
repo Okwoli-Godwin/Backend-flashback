af84df2a9c9dae1a633c7f68229597fe704f17ed
Okwoli-Godwin <okwolig60@gmail.com>
2023-04-02 18:47:27 +0100

    updated

--- App.ts
+++ App.ts
@@ -20,3 +20,24 @@
 //     })
 //     }
 
+import express, {Application, Request, Response} from "express"
+import cors from "cors"
+import morgan from "morgan"
+
+export const appConfig = (app: Application) => {
+    app.use(express.json())
+        .use(cors())
+        .use(morgan("dev"))
+    
+        .get("/", (req: Request, res: Response) => {
+            res.status(200).json({
+            message: "Server is running"
+        })
+
+            app.get("*", (req: Request, res: Response) => {
+                res.status(404).json({
+                    message: `routes nnot found ${req.originalUrl}`
+                })
+            })
+    })
+}
