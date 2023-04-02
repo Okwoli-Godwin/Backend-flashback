f7c8b9378eddfc5aea969d2b2b165c8c88e0743b
Okwoli-Godwin <okwolig60@gmail.com>
2023-04-02 18:13:16 +0100

    updated

--- index.ts
+++ index.ts
@@ -1,14 +1,7 @@
 import express, { Application } from "express"
-import { AppConfig } from "./App"
-import { dbConfig } from "./config/db"
-import { enviromentVariable } from "./enviromentvariable/enviromentvariable"
-
 
 const app: Application = express()
 
-AppConfig(app)
-dbConfig()
-
-app.listen(enviromentVariable.port, () => {
-    console.log("App is running")
+app.listen(3030, () => {
+    console.log("App is up and running")
 })
