61db4fd343e3a95ccbab486a65a9f31cdb4fe2f9
Okwoli-Godwin <okwolig60@gmail.com>
2023-04-02 18:24:34 +0100

    updated

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import express, { Application } from "express"
+import { appConfig } from "./App"
 
 const app: Application = express()
+
+appConfig(app)
 
 app.listen(3030, () => {
     console.log("App is up and running")
