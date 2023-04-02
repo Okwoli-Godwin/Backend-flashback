961b636633f4fd5d0b22c3afb6b80a64658a1905
Okwoli-Godwin <okwolig60@gmail.com>
2023-04-02 15:27:27 +0100

    updated

--- config/db.ts
+++ config/db.ts
@@ -2,11 +2,19 @@ import mongoose from "mongoose";
 import { enviromentVariable } from "../enviromentvariable/enviromentvariable";
 
 const URL = enviromentVariable.LIVE
+// const URL = enviromentVariable.MONGODB_STRING
 
 export const dbConfig = async() => {
     try {
         const conn = await mongoose.connect(URL)
-        console.log(`Database connected to ${conn.connection.host}`)
+        if (
+            conn.connection.host === "localhost"
+        ) {
+            console.log("connected to localhost")
+        }  
+            
+        console.log("Database is live now")
+        
     } catch (error) {
         console.log("Failed to connect to the database", error)
     }
