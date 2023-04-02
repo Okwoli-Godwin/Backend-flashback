e875f0e84f69f0c7978759933bbf4f39f7ac04a1
Okwoli-Godwin <okwolig60@gmail.com>
2023-04-02 18:15:16 +0100

    updated

--- App.ts
+++ App.ts
@@ -1,21 +1,21 @@
-import express, { Application, Request, Response } from "express"
-import cors from "cors"
-import morgan from "morgan"
+// import express, { Application, Request, Response } from "express"
+// import cors from "cors"
+// import morgan from "morgan"
 
-export const AppConfig = (app: Application) => {
-    app.use(express.json())
-        .use(cors())
-        .use(morgan("dev"))
+// export const AppConfig = (app: Application) => {
+//     app.use(express.json())
+//         .use(cors())
+//         .use(morgan("dev"))
     
-        .get("/", (req: Request, res: Response) => {
-            res.status(201).json({
-            message: "Server is up and running"
-        })
-        app.all("*",(req: Request, res: Response) => {
-            res.status(404).json({
-            message: `routes not found ${req.originalUrl}`
-            })
+//         .get("/", (req: Request, res: Response) => {
+//             res.status(201).json({
+//             message: "Server is up and running"
+//         })
+//         app.all("*",(req: Request, res: Response) => {
+//             res.status(404).json({
+//             message: `routes not found ${req.originalUrl}`
+//             })
                 
-        })
-    })
-    }
+//         })
+//     })
+//     }
